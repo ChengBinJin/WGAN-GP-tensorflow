460bea29573a0e3cbee6dc26d69c2a3c11e665e0
chengbin <sbkim0407@gmail.com>
2018-10-16 02:05:52 +0900

    main function

---
+++ src/main.py
@@ -0,0 +1,46 @@
+# ---------------------------------------------------------
+# Tensorflow WGAN-GP Implementation
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Cheng-Bin Jin
+# Email: sbkim0407@gmail.com
+# ---------------------------------------------------------
+import os
+import tensorflow as tf
+from solver import Solver
+
+FLAGS = tf.flags.FLAGS
+
+tf.flags.DEFINE_string('gpu_index', '0', 'gpu index if you have multiple gpus, default: 0')
+tf.flags.DEFINE_integer('batch_size', 50, 'batch size, default: 200')
+tf.flags.DEFINE_string('dataset', 'mnist', 'dataset name from [mnist], default: mnist')
+
+tf.flags.DEFINE_bool('is_train', True, 'training or inference mode, default: True')
+tf.flags.DEFINE_float('learning_rate', 1e-4, 'initial learning rate for Adam, default: 0.0002')
+tf.flags.DEFINE_integer('num_critic', 5, 'the number of iterations of the critic per generator iteration, default: 5')
+tf.flags.DEFINE_integer('z_dim', 128, 'dimension of z vector, default: 128')
+tf.flags.DEFINE_float('lambda', 10., 'hyper-parameter of the gradient penalty, default: 10.')
+tf.flags.DEFINE_float('beta1', 0.5, 'beta1 momentum term of Adam, default: 0.5')
+tf.flags.DEFINE_float('beta2', 0.9, 'beta2 momentum term of Adam, default: 0.9')
+
+
+tf.flags.DEFINE_integer('iters', 200000, 'number of iterations, default: 200000')
+tf.flags.DEFINE_integer('print_freq', 100, 'print frequency for loss, default: 100')
+tf.flags.DEFINE_integer('save_freq', 10000, 'save frequency for model, default: 10000')
+tf.flags.DEFINE_integer('sample_freq', 500, 'sample frequency for saving image, default: 500')
+tf.flags.DEFINE_integer('sample_batch', 200, 'number of sampling images for check generator quality, default: 200')
+tf.flags.DEFINE_string('load_model', None, 'folder of saved model taht you wish to continue training '
+                       '(e.g. 20180907-1739), default: None')
+
+
+def main(_):
+    os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu_index
+
+    solver = Solver(FLAGS)
+    if FLAGS.is_train:
+        solver.train()
+    if not FLAGS.is_train:
+        solver.test()
+
+
+if __name__ == '__main__':
+    tf.app.run()
